10dec72bbe5eb68c234b24eadc881dc183156344
Bhavik Gosrani <bhavik4748@gmail.com>
2021-03-25 17:25:50 -0400

    Added Mumma's Voice

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@
 import { Component } from 'react'
 import './App.css';
 import shusherOgg from './assets/audio/shusher.ogg';
+import mummasvoice from './assets/audio/MummasVoice2.ogg'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <div className="app-label">Shusher App</div>
+        <div className="app-label">Shusher App</div>
+        <div>
+          <audio src={shusherOgg} controls loop />
+        </div>
+
+        <br></br>
+        <br></br>
+
+        <h2> Mumma's Voice</h2>
         <div>
 
-          <audio src={shusherOgg} controls loop />
-
+          <audio src={mummasvoice} controls loop />
         </div>
       </div>
     );
